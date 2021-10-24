de50daa2789935e2c0ca1741d01ed943a4d1e9c4
FedericoA13 <56283793+FedericoA13@users.noreply.github.com>
2021-10-24 18:15:34 -0300

    Crea archivo constantes y tablero

--- etapa1.py
+++ etapa1.py
@@ -1,1 +1,33 @@
-print("Hola")
+import random
+import constantes as const
+
+posicion1 = '[1]'
+posicion2 = '[2]'
+posicion3 = '[3]'
+posicion4 = '[4]'
+
+letra1 = 'X'
+letra2 = 'Z'
+
+lista_fichas = []
+
+
+tablero = [posicion1, posicion2, posicion3, posicion4]
+
+# TODO: Asignar letras a cada posicion de la lista de fichas
+
+print("Fichas y posiciones: ", posicion1, posicion2, posicion3, posicion4)
+pedido = input("Primera posicion: ")
+
+'''
+theBoard = {'top-L': 'O', 'top-M': 'O', 'top-R': 'O', 'mid-L': 'X', 'mid-M':
+'X', 'mid-R': ' ', 'low-L': ' ', 'low-M': ' ', 'low-R': 'X'}
+
+def printBoard(board):
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+printBoard(theBoard)
+'''
