0a592e2a6db12f9e53cae8d0d1c08dc1237684af
FedericoA13 <56283793+FedericoA13@users.noreply.github.com>
2021-10-25 16:26:54 -0300

    Update constantes.py

--- constantes.py
+++ constantes.py
@@ -1,8 +1,3 @@
-<<<<<<< Updated upstream
-FILAS = 4 # este valor es modificable
-COLUMNAS = 4 # este valor es fijo
-=======
 FILAS = 2 # este valor es modificable
 COLUMNAS = 4 # este valor es fijo
->>>>>>> Stashed changes
 VALOR = 4 # valores en cada fila
