55009b9e4258690db6a7064fa58f269f8c2625ed
FedericoA13 <56283793+FedericoA13@users.noreply.github.com>
2021-10-25 16:15:50 -0300

    commit desktop

--- constantes.py
+++ constantes.py
@@ -1,3 +1,8 @@
+<<<<<<< Updated upstream
 FILAS = 4 # este valor es modificable
 COLUMNAS = 4 # este valor es fijo
+=======
+FILAS = 2 # este valor es modificable
+COLUMNAS = 4 # este valor es fijo
+>>>>>>> Stashed changes
 VALOR = 4 # valores en cada fila
