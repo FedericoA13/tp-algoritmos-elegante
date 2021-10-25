1f712fb35b9c90b2c1eb45e3f3f51003cf552c73
FedericoA13 <56283793+FedericoA13@users.noreply.github.com>
2021-10-25 16:22:19 -0300

    Delete constantes.py

--- constantes.py
+++
@@ -1,8 +0,0 @@
-<<<<<<< Updated upstream
-FILAS = 4 # este valor es modificable
-COLUMNAS = 4 # este valor es fijo
-=======
-FILAS = 2 # este valor es modificable
-COLUMNAS = 4 # este valor es fijo
->>>>>>> Stashed changes
-VALOR = 4 # valores en cada fila
