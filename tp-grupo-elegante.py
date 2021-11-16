503008a7a35dc62a9dcb6af071158cccd0e55f33
fm-89 <93018106+fm-89@users.noreply.github.com>
2021-11-16 02:16:18 -0300

    Update tp-grupo-elegante.py

--- tp-grupo-elegante.py
+++ tp-grupo-elegante.py
@@ -80,67 +80,123 @@ def pedir_validar_posiciones(matriz_posiciones_apiladas,contador_posicion):
             validacion_posicion=False
             
     return posicion
-        
+
+def buscar_reemplazar_posiciones(matriz_posiciones_apiladas,matriz_letras_apiladas,posicion):
+    #esta funcion reemplaza el valor que se ve en la pantalla de la posicion con la letra que esta oculta en esa misma posicion
+    
+    #convierto las matrices de posiciones y letras en matrices de 1 sola fila para facilitar operaciones de buscar y encontrar numeros/letras 
+    matriz_letras_apiladas=conversor_matriz(matriz_letras_apiladas)
+    ficha=matriz_letras_apiladas[0][posicion]
+    matriz_posiciones_apiladas=conversor_matriz(matriz_posiciones_apiladas)
+    matriz_posiciones_apiladas[0][posicion]=str(ficha)
+    
+    #reconvierto matrices para que se muestren correctamente en pantalla
+    matriz_letras_apiladas=conversor_matriz(matriz_letras_apiladas)
+    matriz_posiciones_apiladas=conversor_matriz(matriz_posiciones_apiladas)
+    
+    return ficha, matriz_posiciones_apiladas, matriz_letras_apiladas
+    
+def resetear_matriz_posiciones(matriz_posiciones_apiladas,matriz_letras_apiladas,posicion1,posicion2):
+    #si el jugador no acierta la letra con las fichas escogidas esta funcion resetea los valores de la matriz de posiciones
+    #a la posicion que ocupaba la letra (es la inversa de la funcion buscar_reemplazar_posiciones)
+    
+    #creo lista con numeros que son los que van a reemplazar a las letras (notar que tiene el mismo orden que la matriz_posiciones_apiladas)
+    lista_validacion=[x for x in range(1,FILAS*COLUMNAS+1)]
+    
+    matriz_posiciones_apiladas=conversor_matriz(matriz_posiciones_apiladas)
+    matriz_letras_apiladas=conversor_matriz(matriz_letras_apiladas)
+            
+    matriz_posiciones_apiladas[0][posicion1]=lista_validacion[posicion1]
+    matriz_posiciones_apiladas[0][posicion2]=lista_validacion[posicion2]
+            
+    matriz_posiciones_apiladas=conversor_matriz(matriz_posiciones_apiladas)
+    matriz_letras_apiladas=conversor_matriz(matriz_letras_apiladas)
+    
+    return matriz_posiciones_apiladas, matriz_letras_apiladas
+
+def ganador_memotest(lista_intentos_letras_descubiertas,matriz_jugadores):
+    #esta funcion devuelve al ganador del juego si hubo uno o muestra empate
+    if lista_intentos_letras_descubiertas[0][1]>lista_intentos_letras_descubiertas[1][1]:
+        resultado=print("El ganador de la partida es {} con {} pares de fichas encontradas".format(matriz_jugadores[0],lista_intentos_letras_descubiertas[0][1]))
+    elif lista_intentos_letras_descubiertas[0][1]<lista_intentos_letras_descubiertas[1][1]:
+        resultado=print("El ganador de la partida es {} con {} pares de fichas encontradas".format(matriz_jugadores[1],lista_intentos_letras_descubiertas[1][1]))
+    else:
+        if lista_intentos_letras_descubiertas[0][0]<lista_intentos_letras_descubiertas[1][0]:
+            resultado=print ("El ganador de la partida es {}. Ambos encontraron {} pares de fichas pero {} lo hizo en menos intentos".format(matriz_jugadores[0],lista_intentos_letras_descubiertas[0][1],matriz_jugadores[0]))
+        elif lista_intentos_letras_descubiertas[0][0]>lista_intentos_letras_descubiertas[1][0]:
+            resultado=print ("El ganador de la partida es {}. Ambos encontraron {} pares de fichas pero {} lo hizo en menos intentos".format(matriz_jugadores[1],lista_intentos_letras_descubiertas[1][1],matriz_jugadores[1]))
+        else:
+            resultado=print("El juego termino en empate. Ambos encontraron {} pares de fichas en {} intentos".format(int(lista_intentos_letras_descubiertas[0][0]/2),lista_intentos_letras_descubiertas[0][1]))
+    
+    return resultado
+
 def main():
     
-    matriz_posiciones_stackeada, matriz_letras_stackeada=matriz_juego()
-    mostrar_fichas_posiciones(matriz_posiciones_stackeada,matriz_letras_stackeada)
+    print("Bienvenido al juego del Memotest")
+    jugador_1=str(input("Jugador 1, ingrese su nombre: "))
+    jugador_2=str(input("Jugador 2, ingrese su nombre: "))
+    matriz_jugadores=[jugador_1,jugador_2]
+    random.shuffle(matriz_jugadores)
+    turno_jugador_shuffle=matriz_jugadores[0]
+    
+    print("Comenzara jugando:",turno_jugador_shuffle)
+    
+    matriz_posiciones_apiladas, matriz_letras_apiladas=matriz_juego()
+    mostrar_fichas_posiciones(matriz_posiciones_apiladas)
     
     letras_descubiertas=0
-    intentos=0
+    intentos_jugador_shuffle_1=0
+    intentos_jugador_shuffle_2=0
+    letras_descubiertas_jugador_1=0
+    letras_descubiertas_jugador_2=0
+    lista_intentos_letras_descubiertas=[[intentos_jugador_shuffle_1,letras_descubiertas_jugador_1],[intentos_jugador_shuffle_2,letras_descubiertas_jugador_2]]
     
-    while letras_descubiertas<int(const.FILAS*const.COLUMNAS/2):
+    while letras_descubiertas<int(FILAS*COLUMNAS/2):
         contador_posicion=1
-        posicion1=pedir_posiciones(matriz_posiciones_stackeada,contador_posicion)-1
-        
-        #convierto matriz mostrada en pantalla en matriz de 1 fila y asigno operaciones
-        matriz_letras_stackeada=conversor_matriz(matriz_letras_stackeada)
-        ficha1=matriz_letras_stackeada[0][posicion1]
-        matriz_posiciones_stackeada=conversor_matriz(matriz_posiciones_stackeada)
-        matriz_posiciones_stackeada[0][posicion1]=str(ficha1)
-        
-        #reconvierto matrices para volver a mostrar en pantalla
-        matriz_letras_stackeada=conversor_matriz(matriz_letras_stackeada)
-        matriz_posiciones_stackeada=conversor_matriz(matriz_posiciones_stackeada)
-        mostrar_fichas_posiciones(matriz_posiciones_stackeada,matriz_letras_stackeada)
+        posicion1=pedir_validar_posiciones(matriz_posiciones_apiladas,contador_posicion)-1
+        ficha1, matriz_posiciones_apiladas, matriz_letras_apiladas = buscar_reemplazar_posiciones(matriz_posiciones_apiladas,matriz_letras_apiladas,posicion1)
+        mostrar_fichas_posiciones(matriz_posiciones_apiladas)
         
         contador_posicion=2
-        posicion2=pedir_posiciones(matriz_posiciones_stackeada,contador_posicion)-1
-        
-        #convierto matriz mostrada en pantalla en matriz de 1 fila y asigno operaciones
-        matriz_letras_stackeada=conversor_matriz(matriz_letras_stackeada)
-        ficha2=matriz_letras_stackeada[0][posicion2]
-        matriz_posiciones_stackeada=conversor_matriz(matriz_posiciones_stackeada)
-        matriz_posiciones_stackeada[0][posicion2]=str(ficha2)
-        
-        #reconvierto matrices para volver a mostrar en pantalla
-        matriz_letras_stackeada=conversor_matriz(matriz_letras_stackeada)
-        matriz_posiciones_stackeada=conversor_matriz(matriz_posiciones_stackeada)
-        mostrar_fichas_posiciones(matriz_posiciones_stackeada,matriz_letras_stackeada)
-        
-        #se crea lista que retorna a los valores originales las posiciones que el usuario pidio ver
-        lista_validacion=[x for x in range(1,const.FILAS*const.COLUMNAS+1)]
+        posicion2=pedir_validar_posiciones(matriz_posiciones_apiladas,contador_posicion)-1
+        ficha2, matriz_posiciones_apiladas, matriz_letras_apiladas = buscar_reemplazar_posiciones(matriz_posiciones_apiladas,matriz_letras_apiladas,posicion2)
+        mostrar_fichas_posiciones(matriz_posiciones_apiladas)
         
         if ficha1!=ficha2:
-            matriz_posiciones_stackeada=conversor_matriz(matriz_posiciones_stackeada)
-            matriz_letras_stackeada=conversor_matriz(matriz_letras_stackeada)
+            matriz_posiciones_apiladas, matriz_letras_apiladas = resetear_matriz_posiciones(matriz_posiciones_apiladas,matriz_letras_apiladas,posicion1,posicion2)
+        
+            if turno_jugador_shuffle==matriz_jugadores[0]:
+                lista_intentos_letras_descubiertas[0][0]+=1
+                print("Mala suerte",matriz_jugadores[0],"las posiciones seleccionadas no tenian la misma letra. Le toca jugar a",matriz_jugadores[1])
+                turno_jugador_shuffle=matriz_jugadores[1]
+            else:
+                lista_intentos_letras_descubiertas[1][0]+=1
+                print("Mala suerte",matriz_jugadores[1],"las posiciones seleccionadas no tenian la misma letra. Le toca jugar a",matriz_jugadores[0])
+                turno_jugador_shuffle=matriz_jugadores[0]
             
-            matriz_posiciones_stackeada[0][posicion1]=lista_validacion[posicion1]
-            matriz_posiciones_stackeada[0][posicion2]=lista_validacion[posicion2]
-            
-            matriz_posiciones_stackeada=conversor_matriz(matriz_posiciones_stackeada)
-            matriz_letras_stackeada=conversor_matriz(matriz_letras_stackeada)
-            
-            print("Las posiciones seleccionadas no tenian la misma letra. Vuelve a intentarlo")
-            
-            mostrar_fichas_posiciones(matriz_posiciones_stackeada,matriz_letras_stackeada)
-            intentos=intentos+1
+            mostrar_fichas_posiciones(matriz_posiciones_apiladas)
         else:
-            print("Muy bien. Encontraste un par de letras iguales")
-            letras_descubiertas=letras_descubiertas+1
-            intentos=intentos+1
-            
-    print("Felicitaciones. Has encontrado todos los pares de letras. Has ganado.")
-    print("Te ha llevado",intentos,"intentos resolver el memotest")
-            
+            letras_descubiertas+=1
+            if letras_descubiertas<int(FILAS*COLUMNAS/2):
+                if turno_jugador_shuffle==matriz_jugadores[0]:
+                    lista_intentos_letras_descubiertas[0][0]+=1
+                    lista_intentos_letras_descubiertas[0][1]+=1
+                    print("Muy bien",matriz_jugadores[0],"encontraste un par de letras iguales. Puedes continuar jugando")
+                else:
+                    lista_intentos_letras_descubiertas[1][0]+=1
+                    lista_intentos_letras_descubiertas[1][1]+=1
+                    print("Muy bien",matriz_jugadores[1],"encontraste un par de letras iguales. Puedes continuar jugando")
+    
+    if turno_jugador_shuffle==matriz_jugadores[0]:
+        lista_intentos_letras_descubiertas[0][0]+=1
+        lista_intentos_letras_descubiertas[0][1]+=1
+        print("Muy bien",matriz_jugadores[0],"encontraste un par de letras iguales. Ya no quedan mas letras por encontrar")
+    else:
+        lista_intentos_letras_descubiertas[1][0]+=1
+        lista_intentos_letras_descubiertas[1][1]+=1
+        print("Muy bien",matriz_jugadores[1],"encontraste un par de letras iguales. Ya no quedan mas letras por encontrar")
+    
+    print("Felicitaciones. El juego ha terminado")
+    ganador_memotest(lista_intentos_letras_descubiertas,matriz_jugadores)
+    
 main()
